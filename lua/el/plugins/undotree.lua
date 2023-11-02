ea2d2628025a34ac5e9c736e18459b3a7ad27117
ellielle <40385743+ellielle@users.noreply.github.com>
2023-11-02 16:22:58 -0400

    chore: remove keymaps

--- lua/el/plugins/undotree.lua
+++ lua/el/plugins/undotree.lua
@@ -2,6 +2,5 @@ return {
 	"mbbill/undotree",
 	config = function()
 		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle undotree" })
-		vim.keymap.set("n", "<leader>fu", vim.cmd.UndotreeFocus, { desc = "Focus undotree" })
 	end,
 }
