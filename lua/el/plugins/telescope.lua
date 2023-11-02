85ec733c2fc7282a265846218770340669b732af
ellielle <40385743+ellielle@users.noreply.github.com>
2023-11-02 12:26:42 -0400

    fix: telescope fuzzy search working again
    add fuzzy help search

--- lua/el/plugins/telescope.lua
+++ lua/el/plugins/telescope.lua
@@ -10,13 +10,19 @@ return {
 			return
 		end
 
+		local builtin_status, builtin = pcall(require, "telescope.builtin")
+		if not builtin_status then
+			print("telescope.builtin failed to load")
+			return
+		end
+
 		telescope.setup({
 			defaults = {
 				file_ignore_patterns = { "node_modules" },
 				-- hidden, tail, absolute, smart, shorten, truncate (truncate can = a number like "truncate = 3")
 				path_display = { "truncate" },
 				preview = {
-					-- Disable treesitter in the preview window so large files don't hang
+					-- disable treesitter in the preview window so large files don't hang
 					treesitter = false,
 					filesize_limit = 0.1,
 					timeout = 250,
@@ -25,11 +31,13 @@ return {
 		})
 
 		-- set keymaps
-		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
-		vim.keymap.set("n", "<C-p>", "<cmd>Telescope git_files<CR>", { desc = "Fuzzy find files in current git repo" })
-		-- Find string in cwd
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Fuzzy find files in current git repo" })
+		-- find string in cwd
 		vim.keymap.set("n", "<leader>fs", function()
-			telescope.grep_string({ search = vim.fn.input("grep > ") })
-		end)
+			builtin.grep_string({ search = vim.fn.input("grep > ") })
+		end, { desc = "Find any string in cwd" })
+
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Fuzzy find help tags" })
 	end,
 }
