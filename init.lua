c1280847d98e45af0513bcb8aa93c3b871cc8025
ellielle <40385743+ellielle@users.noreply.github.com>
2023-11-03 13:34:39 -0400

    fix: add handler to clear TS errors that hang

--- init.lua
+++ init.lua
@@ -4,11 +4,11 @@ require("el.core.keymaps")
 require("el.lazy") -- load lazy.nvim package manager
 
 -- fix errors not clearing in some cases with TS
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
--- 	underline = true,
--- 	virtual_text = {
--- 		spacing = 5,
--- 		severity_limit = "Warning",
--- 	},
--- 	update_in_insert = true,
--- })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	virtual_text = {
+		spacing = 5,
+		severity_limit = "Warning",
+	},
+	update_in_insert = true,
+})
