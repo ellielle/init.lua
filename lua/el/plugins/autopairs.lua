9b985fa5f46d0f0713239dc5d67df4d4cbaece89
ellielle <40385743+ellielle@users.noreply.github.com>
2023-11-03 15:38:37 -0400

    fix: add config to autopairs to prevent bug with insertion

--- lua/el/plugins/autopairs.lua
+++ lua/el/plugins/autopairs.lua
@@ -1,5 +1,36 @@
 return {
 	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	opts = {},
+	event = { "InsertEnter" },
+	dependencies = {
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		local status, autopairs = pcall(require, "nvim-autopairs")
+		if not status then
+			vim.notify(autopairs, vim.log.levels.ERROR)
+			return
+		end
+
+		local cmpautopairs_status, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+		if not cmpautopairs_status then
+			vim.notify(cmp_autopairs, vim.log.levels.ERROR)
+			return
+		end
+
+		local cmp_status, cmp = pcall(require, "cmp")
+		if not cmp_status then
+			vim.notify(cmp, vim.log.levels.ERROR)
+			return
+		end
+
+		autopairs.setup({
+			check_ts = true,
+			ts_config = {
+				lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+				javascript = { "template_string" }, -- don't add pairs in javascript template string treesitter nodes
+			},
+		})
+
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
