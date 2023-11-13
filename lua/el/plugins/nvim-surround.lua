c7eb142101c968044a9d9483607686d71aecab8d
ellielle <40385743+ellielle@users.noreply.github.com>
2023-11-13 13:33:37 -0500

    fix: call setup for nvim-surround

--- lua/el/plugins/nvim-surround.lua
+++ lua/el/plugins/nvim-surround.lua
@@ -2,5 +2,13 @@ return {
 	"kylechui/nvim-surround",
 	event = { "BufReadPre", "BufNewFile" },
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	config = true,
+	config = function()
+		local status, surround = pcall(require, "nvim-surround")
+		if not status then
+			vim.notify(surround, vim.log.levels.ERROR)
+			return
+		end
+
+		surround.setup({})
+	end,
 }
