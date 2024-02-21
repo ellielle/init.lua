f22f9b9598d8fef6f9b69bc01a0ce986293ef1f4
ellielle <40385743+ellielle@users.noreply.github.com>
2024-02-21 07:11:12 -0500

    it decided it needed to format it differently

--- lua/el/plugins/nvim-cmp.lua
+++ lua/el/plugins/nvim-cmp.lua
@@ -1,80 +1,80 @@
 return {
-	-- completion framework
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		-- LSP completion source
-		"hrsh7th/cmp-nvim-lsp",
+  -- completion framework
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    -- LSP completion source
+    "hrsh7th/cmp-nvim-lsp",
 
-		-- useful completion sources
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-nvim-lua",
-		"hrsh7th/cmp-nvim-lsp-signature-help",
-		"hrsh7th/cmp-buffer",
-		"L3MON4D3/LuaSnip",
-	},
-	config = function()
-		local cmp_status, cmp = pcall(require, "cmp")
-		if not cmp_status then
-			vim.notify(cmp, vim.log.levels.ERROR)
-			return
-		end
-		local luasnip_status, luasnip = pcall(require, "luasnip")
-		if not luasnip_status then
-			vim.notify(luasnip, vim.log.levels.ERROR)
-			return
-		end
+    -- useful completion sources
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-buffer",
+    "L3MON4D3/LuaSnip",
+  },
+  config = function()
+    local cmp_status, cmp = pcall(require, "cmp")
+    if not cmp_status then
+      vim.notify(cmp, vim.log.levels.ERROR)
+      return
+    end
+    local luasnip_status, luasnip = pcall(require, "luasnip")
+    if not luasnip_status then
+      vim.notify(luasnip, vim.log.levels.ERROR)
+      return
+    end
 
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-		cmp.setup({
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
+    cmp.setup({
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
 
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
 
-			mapping = cmp.mapping.preset.insert({
-				-- Enter to confirm completions
-				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+      mapping = cmp.mapping.preset.insert({
+        -- Enter to confirm completions
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
 
-				-- move between completions
-				["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+        -- move between completions
+        ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
 
-				-- ctrl+space to trigger completion menu
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
+        -- ctrl+space to trigger completion menu
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
 
-			-- autocompletion sources
-			sources = cmp.config.sources({
-				{ name = "path", priority = 250 }, -- file paths
-				{ name = "nvim_lsp_signature_help" }, -- function signatures
-				{ name = "nvim_lsp", priority = 1000, keyword_length = 3 }, -- lsp
-				{ name = "nvim_lua", priority = 250, keyword_length = 2 }, -- completion for neovim's Lua runtime API
-				{ name = "buffer", priority = 250, keyword_length = 2 }, -- source current buffer
-				{ name = "LuaSnip", priority = 250, keyword_length = 2 },
-				{ name = "neorg", priority = 250, keyword_length = 2 }, -- neorg plugin for notes/organization
-				{ name = "codeium" },
-			}),
+      -- autocompletion sources
+      sources = cmp.config.sources({
+        { name = "path",                   priority = 250 },    -- file paths
+        { name = "nvim_lsp_signature_help" },                   -- function signatures
+        { name = "nvim_lsp",               priority = 1000, keyword_length = 3 }, -- lsp
+        { name = "nvim_lua",               priority = 250,  keyword_length = 2 }, -- completion for neovim's Lua runtime API
+        { name = "buffer",                 priority = 250,  keyword_length = 2 }, -- source current buffer
+        { name = "LuaSnip",                priority = 250,  keyword_length = 2 },
+        { name = "neorg",                  priority = 250,  keyword_length = 2 }, -- neorg plugin for notes/organization
+        { name = "codeium" },
+      }),
 
-			formatting = {
-				fields = { "menu", "abbr", "kind" },
-				format = function(entry, item)
-					local menu_icon = {
-						nvim_lsp = "λ",
-						vsnip = "⋗",
-						buffer = "Ω",
-						path = "🖫",
-					}
-					item.menu = menu_icon[entry.source.name]
-					return item
-				end,
-			},
-		})
-	end,
+      formatting = {
+        fields = { "menu", "abbr", "kind" },
+        format = function(entry, item)
+          local menu_icon = {
+            nvim_lsp = "λ",
+            vsnip = "⋗",
+            buffer = "Ω",
+            path = "🖫",
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+        end,
+      },
+    })
+  end,
 }
