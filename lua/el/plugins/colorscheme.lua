2d29ac1e01d8479b7b2e2460cb9329e320d2db78
ellielle <40385743+ellielle@users.noreply.github.com>
2024-03-12 13:13:10 -0400

    add tokyodark colorscheme

--- lua/el/plugins/colorscheme.lua
+++ lua/el/plugins/colorscheme.lua
@@ -1,28 +1,16 @@
 return {
-	"rebelot/kanagawa.nvim",
+	"tiagovla/tokyodark.nvim",
 	lazy = false,
-	priority = 1000,
-	config = function()
-		local setup, colorscheme = pcall(require, "kanagawa")
+	opts = {},
+	config = function(_, opts)
+		local setup, colorscheme = pcall(require, "tokyodark")
 		if not setup then
 			vim.notify(colorscheme, vim.log.levels.ERROR)
 			return
 		end
 
-		colorscheme.setup({
-			theme = "wave",
-			-- remove gutter background
-			colors = {
-				theme = {
-					all = {
-						ui = {
-							bg_gutter = "none",
-						},
-					},
-				},
-			},
-		})
+		colorscheme.setup({})
 
-		vim.cmd.colorscheme("kanagawa")
+		vim.cmd.colorscheme("tokyodark")
 	end,
 }
