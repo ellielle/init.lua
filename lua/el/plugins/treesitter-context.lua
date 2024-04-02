a92c1be3ad9c09a48bd74190ab287e2d1fcd23e2
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-02 11:39:51 -0400

    add treesitter context plugin

---
+++ lua/el/plugins/treesitter-context.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  config = function()
+    local context_status, context = pcall(require, "treesitter-context")
+    if not context_status then
+      vim.notify(context, vim.log.levels.ERROR)
+      return
+    end
+
+    vim.keymap.set("n", "[c", function()
+      context.go_to_context()
+    end, { silent = true, desc = "Go to context" })
+  end,
+}
