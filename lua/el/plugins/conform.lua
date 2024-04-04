e63b4bc02a956d7a5f54c5bf2719b15a55f5dfad
ellielle <40385743+ellielle@users.noreply.github.com>
2024-04-04 06:13:09 -0400

    add formatting for yaml

--- lua/el/plugins/conform.lua
+++ lua/el/plugins/conform.lua
@@ -30,6 +30,7 @@ return { -- Autoformat
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
       javascript = { { "prettierd", "prettier" } },
+      yaml = { { "prettierd", "prettier" } },
     },
   },
 }
