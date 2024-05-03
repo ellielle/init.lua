76fcab0e2b44d27beaa84fce09313efa4162fdd6
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-03 14:07:33 -0400

    fix config for pylsp

--- lua/el/plugins/lsp/lspconfig.lua
+++ lua/el/plugins/lsp/lspconfig.lua
@@ -120,25 +120,24 @@ return { -- LSP Configuration & Plugins
       html = {},
       svelte = {},
       templ = {},
-      pylsp = {},
+      pylsp = {
+        settings = {
+          plugins = {
+            flake8 = {
+              enabled = false,
+            },
+            pyflakes = {
+              enabled = false,
+            },
+            pycodestyle = {
+              enabled = false,
+            },
+          },
+        },
+      },
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      --      tsserver = {
-      --        cmd = { "typescript-language-server", "--stdio" },
-      --        filetypes = {
-      --          "javascript",
-      --          "javascriptreact",
-      --          "javascript.jsx",
-      --          "typescript",
-      --          "typescriptreact",
-      --          "typescript.tsx",
-      --        },
-      --      },
       lua_ls = {
         -- cmd = {...},
         -- filetypes = { ...},
