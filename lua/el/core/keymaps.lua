3903a24b3606d091da3f0fee5054f8f0616c8e81
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-13 13:37:36 -0400

    add keymaps for pane resizing

--- lua/el/core/keymaps.lua
+++ lua/el/core/keymaps.lua
@@ -16,12 +16,19 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
+--  Directions mimic vim movement
 --
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+
+-- Keybinds for resizing splits
+vim.keymap.set("n", "<M-h>", "<C-w>5>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<M-l>", "<C-w>5<", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<M-k>", "<C-w>+", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<M-j>", "<C-w>-", { desc = "Move focus to the left window" })
 
 -- Allows shifting highlighted text up and down in visual mode
 -- fails when trying to move beyond first or last line in buffer
