1fbb0881344eb48fff02e5e896ce9ff7d8a95677
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-14 14:35:09 -0400

    update harpoon config to use telescope

--- lua/el/plugins/harpoon.lua
+++ lua/el/plugins/harpoon.lua
@@ -1,12 +1,31 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   config = function()
     local wk = require "which-key"
     local harpoon = require "harpoon"
 
     harpoon:setup()
+
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table {
+            results = file_paths,
+          },
+          previewer = conf.file_previewer {},
+          sorter = conf.generic_sorter {},
+        })
+        :find()
+    end
 
     wk.register {
       a = {
@@ -17,9 +36,9 @@ return {
       },
       ["<C-e>"] = {
         function()
-          harpoon.ui:toggle_quick_menu(harpoon:list())
+          toggle_telescope(harpoon:list())
         end,
-        "Harpoon: Toggle menu",
+        "Harpoon: Open Window",
       },
     }
   end,
