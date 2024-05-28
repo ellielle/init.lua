54b18d20168c28c73522059af47731a37a0fd62c
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-28 17:00:09 -0400

    update lsp stuff

--- lua/el/plugins/treesitter.lua
+++ lua/el/plugins/treesitter.lua
@@ -2,7 +2,21 @@ return { -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   opts = {
-    ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "markdown_inline", "vim", "vimdoc" },
+    ensure_installed = {
+      "javascript",
+      "typescript",
+      "svelte",
+      "python",
+      "go",
+      "bash",
+      "html",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "vim",
+      "vimdoc",
+    },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
