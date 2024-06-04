92bcf8bee434d59a6d9a3af8884732e55803eb4b
ellielle <40385743+ellielle@users.noreply.github.com>
2024-06-04 06:55:10 -0400

    remove tstools, causing errors

--- lua/el/plugins/lsp/tstools.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- typescript-tools to replace tsserver
-  "pmizio/typescript-tools.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  opts = {},
-}
