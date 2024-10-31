59c4894dbf449b1ea9b7b59980cb83c861fc3a04
ellielle <40385743+ellielle@users.noreply.github.com>
2024-10-31 17:27:53 -0400

    disable mouse support

--- lua/el/core/options.lua
+++ lua/el/core/options.lua
@@ -18,7 +18,7 @@ vim.opt.shiftwidth = 4
 vim.opt.wrap = false
 
 -- Enable mouse mode
-vim.opt.mouse = "a"
+vim.opt.mouse = ""
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
